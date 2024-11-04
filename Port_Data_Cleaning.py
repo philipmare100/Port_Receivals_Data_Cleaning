12eb478e3a517b3ab189078a080ce04e97303635
philipmare <philip9.pm@gmail.com>
2024-11-04 15:16:11 +0200

    Added "+02:00"

--- Port_Data_Cleaning.py
+++ Port_Data_Cleaning.py
@@ -116,6 +116,11 @@ if uploaded_file is not None:
                 if col not in mapped_df_for_download.columns:
                     mapped_df_for_download[col] = None
 
+            # Add "+02:00" to all time columns in mapped_df_for_download
+            for col in ["PRN_RECEIVED_DATE","PRN_FORM_COMPLETE"]:  # Specify all columns with time information
+                if col in mapped_df_for_download.columns:
+                            mapped_df_for_download[col] = mapped_df_for_download[col].astype(str) + "+02:00"
+
             # Reorder columns according to column_mappings
             mapped_df_for_download = mapped_df_for_download[column_mappings.values()]
 
